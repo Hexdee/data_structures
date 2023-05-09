738377f364af637f91cb689e1ee318591a3c73be
Hexdee <lukmansaheed79@gmail.com>
2023-05-09 10:19:09 +0100

    Complete question 4

--- week4/question4.js
+++ week4/question4.js
@@ -8,29 +8,31 @@ const rl = readline.createInterface({
 });
 
 rl.question('Enter a string of letters: ', (input) => {
-    const letters = input.split('');
-    const usedLetters = new Set();
+    let letters = input.split('');
     const validWords = [];
 
     for (const word of dictionary) {
-        const lettersCopy = [...letters];
+        let lettersCopy = [...letters];
         let isValidWord = true;
         for (const letter of word) {
-            if (!lettersCopy.includes(letter)) {
+            let letterIndex = lettersCopy.indexOf(letter);
+            if (letterIndex == -1) {
                 isValidWord = false;
                 break;
             } else {
-                usedLetters.add(letter);
-                lettersCopy.splice(lettersCopy.indexOf(letter), 1);
+                lettersCopy.splice(letterIndex, 1);
             }
         }
         if (isValidWord) {
             validWords.push(word);
+            letters = [...lettersCopy];
         }
     }
 
-    console.log(`Valid words: ${validWords.join(', ')}`);
-    console.log(`Unused letters: ${letters.filter(letter => !usedLetters.has(letter)).join(', ')}`);
+    console.log(`Matching words are [${validWords}] unused letters are ‘${letters.join("")}’.`);
+
+    // : ${validWords.join(', ')}`);
+    // console.log(`Unused letters: ${letters.filter(letter => !usedLetters.has(letter)).join(', ')}`);
 
     rl.close();
 });
